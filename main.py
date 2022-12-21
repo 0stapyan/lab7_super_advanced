4db3083ff0174a6e7809f1ce6bc7cbd179b3f302
ostapturash <oturash@kse.org.ua>
2022-12-21 21:23:37 +0200

    task2 and additional

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ parser.add_argument('--medals', action="store_true", required=False, help="")
 parser.add_argument('country', help="")
 parser.add_argument('year', help="")
 parser.add_argument('--output', required=False, help="")
-
+parser.add_argument('--total', action="store_true", required=False, help="")
 args = parser.parse_args()
 filename = args.filename
 medals = args.medals
 country = args.country
 year = args.year
 output = args.output
-
+total = args.total
 def task1(filename, medals, country, year, output):
     if medals:
         output_file = None
@@ -75,6 +75,30 @@ Silver = {silver}
 Bronze = {bronze}""")
         if output_file is not None:
             output_file.close()
+def task2(filename, year, total):
+    if total:
+        first_line = True
+        countries = {}
+        with open(filename, "r") as file:
+            for line in file:
+                data = line.strip().split("\t")
+                country_name = data[6]
+                if first_line:
+                    first_line = False
+                    continue
+                if year == data[9] and data[14] != "NA" and data[6] != "Unified Team":
+                    if str(country_name) not in countries:
+                        countries[str(country_name)] = [0, 0, 0]
+                    if data[14] == "Gold":
+                        countries[str(country_name)][0] += 1
+                    elif data[14] == "Silver":
+                        countries[str(country_name)][1] += 1
+                    elif data[14] == "Bronze":
+                        countries[str(country_name)][2] += 1
+            sorted_countries = sorted(countries.items(), key=lambda x: sum(x[1]), reverse=True)
+            for i in sorted_countries:
+                print(i[0], " ", i[1][0], "Gold", i[1][1], "Silver", i[1][2], "Bronze", " ", "Total", sum(i[1]))
 
 task1(filename, medals, country, year, output)
 total_medals(medals, output)
+task2(filename, year, total)
